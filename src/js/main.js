7a5d2b22264c4825a2fb5d250da3ae24d18e5bbf
Karol Banat <karol.banat@gmail.com>
2022-11-16 16:12:56 +0100

    Map marker icon

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const marker = L.marker([latitude, longitude]).addTo(map);
+const markerIcon = L.icon({
+	iconUrl: './dist/assets/map-marker.png',
+	iconSize: [35, 45],
+	iconAnchor: [0, 50],
+});
+
+const marker = L.marker([latitude, longitude], { icon: markerIcon }).addTo(map);
